8f6795bfabe36bf365e266cc39d5e67d40fe4355
Léo Sumi <leo.sumi@epfl.ch>
2018-12-08 17:49:43 +0100

    Add installation script for Rstudio

---
+++ scripts/wget-rstudio.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# Get Rstudio (Ubuntu 16.04+/Debian 9+ 64-bit)
+# by Léo Sumi
+
+latest_ver=$(curl -s http://download1.rstudio.org/current.ver)
+installed_ver=$(apt-cache show rstudio | grep Version: | cut -d " " -f 2)
+
+# Check the installed version of Rstudio
+if [ "$installed_ver" = "$latest_ver" ]; then
+    echo "Rstudio is already up-to-date ($installed_ver)"
+    exit 0
+fi
+
+# Check if gdebi is installed
+dpkg-query -l gdebi-core > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "gdebi is not installed: install gdebi-core"
+    sudo apt install gdebi-core
+fi
+
+# wget and install Rstudio
+echo "Rstudio is out-of-date"
+echo "current version: $installed_ver"
+echo "latest version:  $latest_ver"
+pushd $HOME/Downloads
+wget -O rstudio.deb https://download1.rstudio.org/rstudio-xenial-$latest_ver-amd64.deb
+sudo gdebi rstudio.deb
+rm rstudio.deb
+popd
