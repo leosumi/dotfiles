6746ae0f599a766690b5e2135460a98d72436626
Léo Sumi <leo.sumi@epfl.ch>
2018-12-16 20:02:57 +0100

    Adapt date format

--- scripts/wget-hosts.sh
+++ scripts/wget-hosts.sh
@@ -15,18 +15,18 @@ pushd /tmp
 wget -O new-hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
 
 # Check for date
-new_hosts_date=$(grep "# Date:" new-hosts | cut -d ":" -f 2)
-cur_hosts_date=$(grep "# Date:" $hosts | cut -d ":" -f 2)
+new_hosts_date=$(grep "# Date:" new-hosts | cut -d " " -f 3,4,5)
+cur_hosts_date=$(grep "# Date:" $hosts | cut -d " " -f 3,4,5)
 if [ "$cur_hosts_date" = "$new_hosts_date" ]; then
-    echo "The hosts file is already up-to-date:$cur_hosts_date"
+    echo "The hosts file is already up-to-date: $cur_hosts_date"
     rm new-hosts
     exit 0
 fi
 
 # Update the current hosts file
 echo "The hosts file is out-of-date"
-echo "current version:$cur_hosts_date"
-echo "latest version:$new_hosts_date"
+echo "current version: $cur_hosts_date"
+echo "latest version: $new_hosts_date"
 sudo cp $hosts_bak $hosts
 sudo cat new-hosts >> $hosts
 rm new-hosts
