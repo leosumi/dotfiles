eaecd399c51c29dae2141d497227425782f79440
Léo Sumi <pro.leosumi@gmail.com>
2019-05-16 21:48:54 +0200

    Fix URL

--- scripts/wget-rstudio.sh
+++ scripts/wget-rstudio.sh
@@ -5,6 +5,9 @@
 latest_ver=$(curl -s http://download1.rstudio.org/current.ver)
 installed_ver=$(apt-cache show rstudio | grep Version: | cut -d " " -f 2)
 
+# Modify latest version
+latest_ver=$(echo "$latest_ver" | cut -d "-" -f 1)
+
 # Check the installed version of Rstudio
 if [ "$installed_ver" = "$latest_ver" ]; then
     echo "Rstudio is already up-to-date ($installed_ver)"
@@ -23,7 +26,7 @@ echo "Rstudio is out-of-date"
 echo "current version: $installed_ver"
 echo "latest version:  $latest_ver"
 pushd /tmp
-wget -O rstudio.deb https://download1.rstudio.org/rstudio-xenial-$latest_ver-amd64.deb
+wget -O rstudio.deb https://download1.rstudio.org/desktop/bionic/amd64/rstudio-$latest_ver-amd64.deb
 sudo gdebi rstudio.deb
 rm rstudio.deb
 popd
